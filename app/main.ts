1714558a25bac04a061c892eec468b8bbea0e19f
Vincent Lam <vincent@formfluid.com>
2025-12-01 19:33:46 +1030

    feature: added invalid commands

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,7 @@ const rl = createInterface({
 });
 
 // TODO: Uncomment the code below to pass the first stage
-rl.question("$ ", (answer) => {
+rl.question("$ ", (command: string) => {
+  console.log(`${command}: command not found`);
   rl.close();
 });
