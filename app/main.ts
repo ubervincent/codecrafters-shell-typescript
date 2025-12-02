fc37eb70030810fe4e4b9307e9e21492664cc7a8
Vincent Lam <vincent@formfluid.com>
2025-12-02 22:01:15 +1030

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -11,10 +11,14 @@ rl.prompt()
 rl.on('line', (line: string) => {
   if (line == "exit") {
     rl.close()
+  } else if (line.split(" ")[0] == "echo") {
+    const echo = line.split(" ")
+    echo.shift()
+    console.log(`${echo.join(" ")}`)
+    rl.prompt()
   } else {
     console.log(`${line}: command not found`);
     rl.prompt()
   }
-  
 });
 
