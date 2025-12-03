38c568dccdea2bf4cae294f912abde5178bf81c8
Vincent Lam <vincent@formfluid.com>
2025-12-03 18:35:29 +1030

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -9,15 +9,36 @@ rl.setPrompt("$ ")
 rl.prompt()
 
 rl.on('line', (line: string) => {
+  const parts = line.split(" ")
+  const command = parts[0]
+
   if (line == "exit") {
+
     rl.close()
-  } else if (line.split(" ")[0] == "echo") {
+
+  } else if (command == "echo") {
+
     const echo = line.split(" ").slice(1)
     console.log(`${echo.join(" ")}`)
     rl.prompt()
+
+  } else if (command == "type") {
+
+    const toCheck = parts.slice(1).join(" ")
+
+    if (toCheck == "exit" || toCheck == "type" || toCheck == "echo") {
+      console.log(`${toCheck} is a shell builtin`)
+      rl.prompt()
+    } else {
+      console.log(`${toCheck} not found`)
+      rl.prompt()
+    }
+
   } else {
+
     console.log(`${line}: command not found`);
     rl.prompt()
+
   }
 });
 
