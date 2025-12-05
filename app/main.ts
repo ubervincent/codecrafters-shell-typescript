91d181c187b6f5638a27bde3e2d122b1b2f9c238
Vincent Lam <vincent@formfluid.com>
2025-12-05 19:40:15 +1030

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
-import { createInterface } from "readline";
+import { createInterface } from "node:readline";
+import path from 'node:path';
 
 const rl = createInterface({
   input: process.stdin,
@@ -19,9 +20,22 @@ rl.on('line', (line: string) => {
     console.log(`${echo.join(" ")}`)
 
   } else if (command == "type") {
-    ["echo", "type", "exit"].includes(args[0]) ? console.log(`${args[0]} is a shell builtin`) : console.log(`${args[0]}: not found`)
+    const files = process.env.PATH?.split(path.delimiter)
+    let found = false
+
+    for (const filePath of files) {
+      if (path.basename(filePath) == args[0]) {
+        console.log(`${args[0]} is ${filePath}`)
+        found = true
+      } 
+    }
+    
+    if (!found) {
+      ["echo", "type", "exit"].includes(args[0]) ? console.log(`${args[0]} is a shell builtin`) : console.log(`${args[0]}: not found`)
+    }
 
   } else {
+
     console.log(`${line}: command not found`);
 
   }
